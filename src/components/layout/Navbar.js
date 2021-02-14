a1c43a23a3132b2cdbce43206720b6782b740065
Leung Ka Kin <kakin.leung@student.kdg.be>
2021-02-14 23:15:01 +0100

    derp

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Navbar = ({ title }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Navbar;
